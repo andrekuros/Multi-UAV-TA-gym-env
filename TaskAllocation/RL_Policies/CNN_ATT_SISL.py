aac3f03142cfce3f7ebf306d66f5f27dfc474cbf
andrekuros <andrekuros@gmail.com>
2023-12-08 11:36:05 -0500

    With positional encoder

--- TaskAllocation/RL_Policies/CNN_ATT_SISL.py
+++ TaskAllocation/RL_Policies/CNN_ATT_SISL.py
@@ -3,6 +3,23 @@ import torch.nn as nn
 from tianshou.utils.net.common import Net
 import numpy as np
 
+def positional_encoding(seq_len, d_model, device):
+        """
+        Generates and returns positional encodings.
+        
+        :param seq_len: Length of the state memory sequence.
+        :param d_model: The dimension of the embeddings/feature vectors.
+        :param device: The device to which tensors will be sent.
+        :return: A tensor of shape [seq_len, d_model] representing the positional encodings.
+        """
+        position = np.arange(seq_len)[:, np.newaxis]
+        div_term = np.exp(np.arange(0, d_model, 2) * -(np.log(10000.0) / d_model))
+        pe = np.zeros((seq_len, d_model))
+        pe[:, 0::2] = np.sin(position * div_term)
+        pe[:, 1::2] = np.cos(position * div_term)
+        return torch.tensor(pe, dtype=torch.float32).to(device)
+
+
 class CNN_ATT_SISL(Net):
     def __init__(self, obs_shape, action_shape, device):
         super().__init__(state_shape=0, action_shape=action_shape, device=device)
@@ -39,9 +56,9 @@ class CNN_ATT_SISL(Net):
         state_memory = obs[:, 1:, :, :]  # State memory are the subsequent channels
 
         # Generate positional encodings
-        # seq_len = state_memory.shape[1]  # Assuming state_memory shape is [batch, seq_len, height, width, channels]
-        # d_model = 64  # Assuming feature size after CNN is 64
-        # pos_encodings = positional_encoding(seq_len, d_model, self.device)
+        seq_len = state_memory.shape[1]  # Assuming state_memory shape is [batch, seq_len, height, width, channels]
+        d_model = 64  # Assuming feature size after CNN is 64
+        pos_encodings = positional_encoding(seq_len, d_model, self.device)
 
 
         # Remove the channel dimension from current_obs since it's only one channel
@@ -68,18 +85,13 @@ class CNN_ATT_SISL(Net):
 
         # Reshape back to original memory shape with features
         state_memory_features = state_memory_features.view(batch_size, memory_channels, -1)
-        # print(" state_memory_features2" , state_memory_features.shape)
-                
-        # Prepare state memory for attention mechanism
-        # state_memory_features = state_memory_features.permute(1, 0, 2)  # Adjust dimensions to [seq_len, batch, features]
-        # print(" state_memory_features3" , state_memory_features.shape)
         
-
-        # Add positional encodings to state memory features
+        # # Add positional encodings to state memory features
         # state_memory_features = state_memory_features.view(batch_size, seq_len, -1)
-        # state_memory_features += pos_encodings.unsqueeze(0)
+        # print("state_memory_features.shape", state_memory_features.shape, state_memory_features)
+        state_memory_features = state_memory_features + pos_encodings.unsqueeze(0)
         
-        # print("state_memory_features.shape", state_memory_features.shape)
+        # print("state_memory_features.shape", state_memory_features.shape, state_memory_features)
 
         # Apply attention
         attn_output, weights = self.attention(current_state_features, state_memory_features, state_memory_features)
@@ -99,24 +111,10 @@ class CNN_ATT_SISL(Net):
         # print("policy_output", policy_output)
         # print("value_output", value_output.shape)
 
-        return policy_output.squeeze(0), None#value_output.squeeze(0)  # Adjust dimensions if necessary
+        return policy_output, None#value_output.squeeze(0)  # Adjust dimensions if necessary
 
 
     def value_function(self):
         return self._value_out.flatten()
     
-    def positional_encoding(seq_len, d_model, device):
-        """
-        Generates and returns positional encodings.
-        
-        :param seq_len: Length of the state memory sequence.
-        :param d_model: The dimension of the embeddings/feature vectors.
-        :param device: The device to which tensors will be sent.
-        :return: A tensor of shape [seq_len, d_model] representing the positional encodings.
-        """
-        position = np.arange(seq_len)[:, np.newaxis]
-        div_term = np.exp(np.arange(0, d_model, 2) * -(np.log(10000.0) / d_model))
-        pe = np.zeros((seq_len, d_model))
-        pe[:, 0::2] = np.sin(position * div_term)
-        pe[:, 1::2] = np.cos(position * div_term)
-        return torch.tensor(pe, dtype=torch.float32).to(device)
+    
