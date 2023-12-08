6d71ac9d87d1f02451c8f207c87d224c98b00f37
andrekuros <ANDREKUROS@GMAIL.COM>
2023-12-08 00:19:40 -0500

    Mem adjusted

--- Mods/MemPursuitEnv.py
+++ Mods/MemPursuitEnv.py
@@ -5,48 +5,59 @@ from pettingzoo.utils import agent_selector, wrappers
 
 class MemPursuitEnv(pursuit_v4.raw_env):
     
-    def __init__(self, memory_size=10, *args, **kwargs):
+    def __init__(self, att_memory = False, memory_size=5, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
         self.memory_size = memory_size
+        self.att_memory = att_memory
         # Initialize state memory for each agent
-        self.state_memories = {agent: deque(maxlen=self.memory_size) for agent in self.agents}
+        if att_memory:
+            self.state_memories = {agent: deque(maxlen=self.memory_size) for agent in self.agents}
         
 
     def step(self, action):
         super().step(action)
-        # Update state memory for all agents
-        for agent in self.agents:
+        
+        if self.att_memory:
+            
+            # Update state memory agents
+            agent = self.agent_selection        
             agent_obs = self.env.safely_observe(self.agent_name_mapping[agent])
+            
             # Ensure the observation is correctly formatted for memory storage
             self.state_memories[agent].append(np.swapaxes(agent_obs, 0, 2))
-            # print("self.state_memories:", self.state_memories)
+            
 
     def observe(self, agent):
         current_obs = super().observe(agent)  # This already returns a correctly formatted observation
-        state_memory = np.array(list(self.state_memories[agent]))
-        if state_memory.shape[0] < self.memory_size:
-            # Pad the state memory if it's not yet full
-            pad_width = self.memory_size - state_memory.shape[0]
-            # print("Before padding:", state_memory.shape)
-            state_memory = np.pad(state_memory, ((pad_width, 0), (0, 0), (0, 0), (0, 0)), mode='constant')
         
-        # print("Current", current_obs[np.newaxis, ...].shape)
-        # print("state_memory", state_memory.shape)
+        if self.att_memory:
+            state_memory = np.array(list(self.state_memories[agent]))
+            if state_memory.shape[0] < self.memory_size:
+                # Pad the state memory if it's not yet full
+                pad_width = self.memory_size - state_memory.shape[0]
+                # print("Before padding:", state_memory.shape)
+                state_memory = np.pad(state_memory, ((pad_width, 0), (0, 0), (0, 0), (0, 0)), mode='constant')
 
-        combined_obs = np.concatenate((current_obs[np.newaxis, ...], state_memory), axis=0)
-        return combined_obs
+            combined_obs = np.concatenate((current_obs[np.newaxis, ...], state_memory), axis=0)
+            return combined_obs
+
+        else:
+
+            return current_obs
     
     def reset(self, seed=None, options=None):
         # Call the base environment's reset
         super().reset(seed=seed, options=options)
         
-        # Reset the state memories for each agent
-        for agent in self.agents:
-            self.state_memories[agent].clear()
-            # Optionally, you can initialize the state memory with a starting state
-            initial_state = np.zeros((self.observation_spaces[agent].shape[0], self.observation_spaces[agent].shape[1], self.observation_spaces[agent].shape[2]))
-            for _ in range(self.memory_size):
-                self.state_memories[agent].append(initial_state)
+        if self.att_memory:
+            # Reset the state memories for each agent
+            for agent in self.agents:
+                self.state_memories[agent].clear()
+                # Optionally, you can initialize the state memory with a starting state
+                initial_state = np.zeros((self.observation_spaces[agent].shape[0], self.observation_spaces[agent].shape[1], self.observation_spaces[agent].shape[2]))
+                for _ in range(self.memory_size):
+                    self.state_memories[agent].append(initial_state)
 
 
 def env(**kwargs):
--- TaskAllocation/RL_Policies/CNN_ATT_SISL.py
+++ TaskAllocation/RL_Policies/CNN_ATT_SISL.py
@@ -4,10 +4,9 @@ from tianshou.utils.net.common import Net
 import numpy as np
 
 class CNN_ATT_SISL(Net):
-    def __init__(self, obs_shape, action_shape, device, memory_size=10):
+    def __init__(self, obs_shape, action_shape, device):
         super().__init__(state_shape=0, action_shape=action_shape, device=device)
-        self.memory_size = memory_size
-
+        
         # CNN model for processing current state
         self.cnn_model = nn.Sequential(
             nn.Conv2d(3, 32, [2, 2]),
@@ -22,13 +21,14 @@ class CNN_ATT_SISL(Net):
         )
 
         # Multi-head attention mechanism
-        self.attention = nn.MultiheadAttention(embed_dim=64, num_heads=4)
-        self.attention_fc = nn.Linear(64, 64)
+        self.attention = nn.MultiheadAttention(embed_dim=64, num_heads=4, batch_first=True).to(device)
+        self.attention_fc = nn.Linear(64, 64).to(device)
 
         # Decision making layers
-        self.policy_fn = nn.Linear(64, 5)
-        self.value_fn = nn.Linear(64, 1)
+        self.policy_fn = nn.Linear(64, 5).to(device)
+        self.value_fn = nn.Linear(64, 1).to(device)
 
+    
     def forward(self, obs, state=None, info=None):
         # Convert obs to tensor if it is a numpy array
         if isinstance(obs, np.ndarray):
@@ -38,12 +38,20 @@ class CNN_ATT_SISL(Net):
         current_obs = obs[:, :1, :, :]  # Current obs is the first channel
         state_memory = obs[:, 1:, :, :]  # State memory are the subsequent channels
 
+        # Generate positional encodings
+        # seq_len = state_memory.shape[1]  # Assuming state_memory shape is [batch, seq_len, height, width, channels]
+        # d_model = 64  # Assuming feature size after CNN is 64
+        # pos_encodings = positional_encoding(seq_len, d_model, self.device)
+
+
         # Remove the channel dimension from current_obs since it's only one channel
         current_obs = current_obs.squeeze(1)
         # Process current observation through CNN
         current_obs_permuted = current_obs.permute(0, 3, 1, 2) 
         # print(" current_obs_permuted" , current_obs_permuted.shape)  
-        current_state_features = self.cnn_model(current_obs_permuted)
+        
+        current_state_features = self.cnn_model(current_obs_permuted).unsqueeze(1)
+        # print("current_state_features.shape", current_state_features.shape)
         
         # Process each state memory observation through the CNN
         # print(" MemoryShape" , state_memory.shape)
@@ -53,6 +61,7 @@ class CNN_ATT_SISL(Net):
         state_memory_permuted = state_memory.permute(0, 1, 3, 2)  # Adjust dimensions to match CNN input (N, C, H, W)
         # state_memory_permuted = state_memory.permute(0, 3, 1, 2)  # Adjust dimensions to match CNN input
         # print(" MemoryShape3" , state_memory_permuted.shape)
+        
 
         # Process through CNN
         state_memory_features = self.cnn_model(state_memory_permuted)
@@ -62,25 +71,52 @@ class CNN_ATT_SISL(Net):
         # print(" state_memory_features2" , state_memory_features.shape)
                 
         # Prepare state memory for attention mechanism
-        state_memory_features = state_memory_features.permute(1, 0, 2)  # Adjust dimensions to [seq_len, batch, features]
-        # print(" state_memory_features2" , state_memory_features.shape)
+        # state_memory_features = state_memory_features.permute(1, 0, 2)  # Adjust dimensions to [seq_len, batch, features]
+        # print(" state_memory_features3" , state_memory_features.shape)
+        
+
+        # Add positional encodings to state memory features
+        # state_memory_features = state_memory_features.view(batch_size, seq_len, -1)
+        # state_memory_features += pos_encodings.unsqueeze(0)
+        
+        # print("state_memory_features.shape", state_memory_features.shape)
 
         # Apply attention
-        attn_output, _ = self.attention(current_state_features.unsqueeze(0), state_memory_features, state_memory_features)
+        attn_output, weights = self.attention(current_state_features, state_memory_features, state_memory_features)
         attn_output = self.attention_fc(attn_output.squeeze(0))
 
+        # print("weights:", weights.shape)
         # Combine CNN output and attention output
         combined_output = current_state_features + attn_output
-
+        # print("att_out",attn_output.shape)
         # Compute policy and value
-        policy_output = self.policy_fn(combined_output)
+        policy_output = self.policy_fn(combined_output).squeeze(1)
+        # policy_output = torch.squeeze(policy_output, -1).to(self.device) 
+
         value_output = self.value_fn(combined_output)
 
         # print("policy_output", policy_output.shape)
+        # print("policy_output", policy_output)
         # print("value_output", value_output.shape)
 
-        return policy_output, None#value_output.squeeze(0)  # Adjust dimensions if necessary
+        return policy_output.squeeze(0), None#value_output.squeeze(0)  # Adjust dimensions if necessary
 
 
     def value_function(self):
         return self._value_out.flatten()
+    
+    def positional_encoding(seq_len, d_model, device):
+        """
+        Generates and returns positional encodings.
+        
+        :param seq_len: Length of the state memory sequence.
+        :param d_model: The dimension of the embeddings/feature vectors.
+        :param device: The device to which tensors will be sent.
+        :return: A tensor of shape [seq_len, d_model] representing the positional encodings.
+        """
+        position = np.arange(seq_len)[:, np.newaxis]
+        div_term = np.exp(np.arange(0, d_model, 2) * -(np.log(10000.0) / d_model))
+        pe = np.zeros((seq_len, d_model))
+        pe[:, 0::2] = np.sin(position * div_term)
+        pe[:, 1::2] = np.cos(position * div_term)
+        return torch.tensor(pe, dtype=torch.float32).to(device)
